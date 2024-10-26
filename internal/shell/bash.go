8ecc0fb8cee5aaabfccc2b6cdf54776aebd798f7
Johannes Beisiegel <jbe@xfoo.eu>
2024-10-26 12:33:06 +0200

    use template for building rc-file

--- internal/shell/bash.go
+++ internal/shell/bash.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"text/template"
 
 	"github.com/j-b-e/ossie/internal/config"
 	"github.com/j-b-e/ossie/internal/model"
@@ -12,35 +13,48 @@ import (
 )
 
 func bashRC(cloud model.Cloud) string {
-	export := envToExport(cloud)
-	var env strings.Builder
-
-	env.WriteString(`
+	const rcTempl = `
 if [[ -f "/etc/bash.bashrc" ]] ; then
   source "/etc/bash.bashrc"
 fi
 if [[ -f "$HOME/.bashrc" ]] ; then
   source "$HOME/.bashrc"
 fi
-`)
-	env.WriteString(export)
-	env.WriteString("function _ossie_exec_ () {\n")
-	if config.Global.ProtectEnv {
-		env.WriteString("unset ${!OS_*}\n")
-		env.WriteString(export)
-	}
-	env.WriteString("export " + config.NestedEnvKey + "=" + config.NestedEnvVal + "\n}\n")
-	if config.Global.Aliases {
-		env.WriteString("alias os=openstack; alias o=openstack;\n")
-	}
-	env.WriteString(`function osenv () {
-	echo -n '` + strings.ReplaceAll(export, "OS_PASSWORD="+cloud.Env["OS_PASSWORD"], "OS_PASSWORD=****") + `'
+{{ .export }}
+function _ossie_exec_ () {
+{{ if .protectenv -}}
+unset ${!OS_*}
+{{ .export }}
+{{ end -}}
+export {{ .nested_marker }}
+}
+{{ if .aliases -}}
+alias os=openstack
+alias o=openstack
+{{ end -}}
+function osenv () {
+	echo -n '{{ .export_safe }}'
 }
 trap '_ossie_exec_' DEBUG
 _ossie_OLDPS="$PS1"
-PS1="[` + generatePrompt(cloud) + `]$_ossie_OLDPS"
-`)
-	return env.String()
+PS1="[{{ .prompt }}]$_ossie_OLDPS"
+`
+	var out strings.Builder
+	t := template.Must(template.New("rc").Parse(rcTempl))
+	env := envToExport(cloud)
+	data := map[string]any{
+		"export":        env,
+		"export_safe":   strings.ReplaceAll(env, cloud.Env["OS_PASSWORD"], "****"),
+		"nested_marker": config.NestedEnvKey + "=" + config.NestedEnvVal,
+		"prompt":        generatePrompt(cloud),
+		"protectenv":    config.Global.ProtectEnv,
+		"aliases":       config.Global.Aliases,
+	}
+	err := t.Execute(&out, data)
+	if err != nil {
+		panic(err)
+	}
+	return out.String()
 }
 
 func SpawnBash(cloud model.Cloud) {
